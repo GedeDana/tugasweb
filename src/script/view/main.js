57c891dbb8f6f50303c9e504a80e4ca4c39a9bd2
GedeDana <73019594+GedeDana@users.noreply.github.com>
2021-10-27 16:36:48 +0800

    add function Indo main,render,fallback
    DOM statik covd-19 idn

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -50,3 +50,51 @@ const main = function () {
 
     buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
+const indoMain = function() {
+    const indonesiCovidElement = document.querySelector("#indoCovid");
+
+    const render = function(){
+        const dataSource = new DataSource(renderResult, fallbackResult);
+        dataSource.renderCovidInfo();
+    }
+    let renderResult = function (idnCovid19) {
+        const positif = idnCovid19.positif;
+        const negatif = idnCovid19.negatif;
+        const meninggal = idnCovid19.meninggal;
+        const title = idnCovid19.nama;
+
+        const covidTitleElement = document.createElement("div");
+        covidTitleElement.setAttribute("class", "title");
+
+        const covidInformation = document.createElement("div");
+        covidInformation.setAttribute("class", "container-box");
+
+            covidInformation.innerHTML = `
+            <div class="box blue">
+            <h3>Negatif</h3>
+            <p>${negatif}</p>
+        </div>
+        <div class="box yellow">
+            <h3>Positif</h3>
+            <p>${positif}</p>
+        </div>
+        <div class="box red">
+            <h3>Meninggal</h3>
+            <p>${meninggal}</p>
+        </div>`;
+
+        covidTitleElement.innerHTML = `<h3>${title}</h3>`;
+        indonesiCovidElement.appendChild(covidTitleElement);
+        indonesiCovidElement.appendChild(covidInformation);
+
+    }
+
+    let fallbackResult = function () {
+        indonesiCovidElement.innerHTML = " ";
+        indonesiCovidElement.innerHTML += `<h2>Data Covid Tidak ketemu</h2>`;
+    };
+
+    indonesiCovidElement.addEventListener("load",render)
+
+}
+
